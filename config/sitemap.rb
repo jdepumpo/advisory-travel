1b3fed59468b18c401fb26d8489ab9e08650b21d
Joe DePumpo <joe@depumpo.com>
2024-04-20 19:31:58 -0400

    Add sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,11 +1,14 @@
+require 'rubygems'
+require 'sitemap_generator'
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://advisory.travel"
 
 SitemapGenerator::Sitemap.create do
   add '/countries'
   add '/map'
-  Countries.find_each do |country|
-    add country_path(country), :lastmod => country.advisories.last.pub_date
+  Country.find_each do |country|
+    add country_path(country.alpha2)
   end
 
 end
